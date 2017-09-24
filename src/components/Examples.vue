90f94767232e052418f62c2965227d30c8720f29
Alex Efremov <alex.efremov.dev@gmail.com>
2017-09-24 16:39:30 +0300

    Add example for ep3 State and Props

---
+++ src/components/Examples.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="examples">
+    <h3>Пример 1 - Освещение</h3>
+    <lighting />
+    <h3>Пример 2 - Пользователи</h3>
+    <users />
+  </div>
+</template>
+
+<script>
+import Users from '@/components/Users';
+import Lighting from '@/components/Lighting';
+
+export default {
+  name: 'examples',
+  components: {
+    Users,
+    Lighting,
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+</style>
---
+++ src/components/Room.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="room">
+    <h4>{{ title }}</h4>
+    <div :class="{ indicator: true, active: isLight}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rooms',
+  props: {
+    isLight: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .room {
+    width: 100px;
+    text-align: center;
+  }
+
+  .indicator {
+    height: 20px;
+    width: 20px;
+    background-color: #ccc;
+    border-radius: 50%;
+    margin: 0 auto;
+  }
+
+  .active {
+    background-color: #f9e814;
+  }
+</style>
