26a059c4c18dd122c4e0f135437f1b1884eae1b8
Alex Efremov <alex.efremov.dev@gmail.com>
2017-09-30 18:55:20 +0300

    Add News and About components

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="news">
+    About page: Info and contacts
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/News.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="news">
+    News about the company
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'news',
+};
+</script>
+
+<style scoped>
+</style>
